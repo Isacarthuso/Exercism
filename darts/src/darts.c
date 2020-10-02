8cb5a6e261b2a1e9bea87ae85f8113bbaf30462b
Isac <isacarthuso@gmail.com>
2020-10-03 01:57:03 +0200

    add darts

---
+++ darts/src/darts.c
@@ -0,0 +1,16 @@
+#include "darts.h"
+uint8_t score(coordinate_t dart_at)
+{
+	float hyp;
+
+	hyp = sqrtf(pow(dart_at.x, 2) + pow(dart_at.y, 2));
+
+	if (hyp <= 1)
+		return 10;
+	else if (hyp > 1 && hyp <= 5)
+		return 5;
+	else if (hyp > 5 && hyp <= 10)
+		return 1;
+	else
+		return 0;
+}
